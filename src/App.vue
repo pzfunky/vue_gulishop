1dbdef95b87a3d3106424d6e0f4fb4d525c4147c
PierceZhang <pzfunky@outlook.com>
2021-01-27 08:26:30 +0800

    gengxin

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  
+    <div>
+        <Header></Header>
+        <router-view></router-view>
+        <!-- 这样写是可以的,但是如果后期有很多的页面都不需要显示Footer,那么这里就会写很长 -->
+        <!-- <Footer v-show="$route.path !== '/login' && $route.path !== '/register'"></Footer> -->
+        <Footer v-show="!$route.meta.isHidden"></Footer>
+    </div>
 </template>
 
 <script>
-    export default {
+    import Header from '@/components/Header'
+    import Footer from '@/components/Footer'
 
+    export default {
+        name:'App',
+        components:{
+            Header,
+            Footer
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 // import App from './App.vue'
 import App from '@/App'
 //@是一个别名,是一个小名,代表的就是src的路径
+import router from '@/router/index'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router  //我们所有的组件内部都可以使用this.$router 和 this.$route
 }).$mount('#app')
